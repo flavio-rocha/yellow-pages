e606b676a4d08977c2a4c32788247228a3fde514
Flávio Rocha <rocha.flavio.pt@gmail.com>
2024-10-10 22:47:26 +0100

    server able to launch (#1)

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: false,
+  optionsSuccessStatus: 204,
+};
+
+// middleware
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+// routes
+app.use(routes.peopleRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
